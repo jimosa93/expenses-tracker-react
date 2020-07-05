9a78650b8e59bbb84c95d9765650a0fbbca6f497
Jorge Morales <jimosa93@gmail.com>
2020-07-04 21:07:44 -0500

    Add backend app

---
+++ client/src/components/Transaction.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/format";
+
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? "-" : "+";
+  return (
+    <div>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${numberWithCommas(Math.abs(transaction.amount))}
+        </span>
+        <button
+          onClick={() => deleteTransaction(transaction._id)}
+          className="delete-btn"
+        >
+          x
+        </button>
+      </li>
+    </div>
+  );
+};
